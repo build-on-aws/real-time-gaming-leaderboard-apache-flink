e18f70a3c85a9e36c3050b739f554c19433ec36f
Anand Shah <annshah@amazon.lu>
2023-08-02 21:52:58 +0200

    Redis command fix

--- infra/functions/redis-sync/app.py
+++ infra/functions/redis-sync/app.py
@@ -10,7 +10,7 @@ r = redis.RedisCluster(host=os.environ["REDIS_HOST"], port=6379)
 def lambda_handler(event, context):
     print('Incoming event: ', event)
     for record in event["Records"]:
-        decoded_json = base64.decodestring(record["kinesis"]["data"])
-        command = json.load(decoded_json)["command"]
-        data = r.execute_command(command)
+        bytes = base64.decodebytes(str.encode(record["kinesis"]["data"]))
+        command = json.loads(bytes)["command"]
+        data = r.execute_command(*command.split(" "))
         print(data)
