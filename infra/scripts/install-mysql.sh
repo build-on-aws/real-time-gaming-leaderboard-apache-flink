17fb5f38a3dc72890fe3be5eae806024ca5fb89c
Anand Shah <annshah@amazon.lu>
2024-08-29 16:09:05 +0200

    Stick to MySQL 8.0 for binlog feature

--- infra/scripts/install-mysql.sh
+++ infra/scripts/install-mysql.sh
@@ -5,4 +5,4 @@ export PASSWORD=$(aws secretsmanager get-secret-value --secret-id "$1" --region 
 sudo mkdir -p /home/ec2-user/init
 sudo chmod -R 777 /home/ec2-user/init
 echo "GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* to admin@'%';" >> /home/ec2-user/init/grants.sql
-sudo docker run -d -p 3306:3306 --name db -e MYSQL_USER=admin -e MYSQL_DATABASE=gaming -e MYSQL_PASSWORD="$PASSWORD" -e MYSQL_ROOT_PASSWORD="$PASSWORD" -v /home/ec2-user/init:/docker-entrypoint-initdb.d mysql:8
+sudo docker run -d -p 3306:3306 --name db -e MYSQL_USER=admin -e MYSQL_DATABASE=gaming -e MYSQL_PASSWORD="$PASSWORD" -e MYSQL_ROOT_PASSWORD="$PASSWORD" -v /home/ec2-user/init:/docker-entrypoint-initdb.d mysql:8.0
